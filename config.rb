e5ba32cd3eaae0c7a8318c4c40b5c6da92fc5d88
Curtis Blackwell <blackwell.curtis@gmail.com>
2013-03-11 00:45:01 +0900

    removed yeoman stuff, was experiencing a weird issue and this is too simple to deal with that shit

--- config.rb
+++ config.rb
@@ -1,14 +1,14 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/app"
+http_path = "/"
 css_dir = "styles"
 sass_dir = "styles"
 images_dir = "images"
-# javascripts_dir = "javascripts"
+javascripts_dir = "scripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
